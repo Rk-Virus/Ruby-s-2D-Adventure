6483320ddc93e5d90acf4eef2840295e5b42a9df
Ravi Mishra <ravim70655@gmail.com>
2024-02-24 17:12:51 +0530

    added physics

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -5,22 +5,42 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public float speed = 2.0f;
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    public int maxHealth = 5;
+    public int health { get { return currentHealth; }}
+    int currentHealth;
+
     // Start is called before the first frame update
     void Start()
     {
         // QualitySettings.vSyncCount = 0;
         // Application.targetFrameRate = 10;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Debug.Log(horizontal);
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
         Vector2 position = transform.position;
         position.x = position.x + speed * horizontal * Time.deltaTime;
         position.y = position.y + speed * vertical * Time.deltaTime;
-        transform.position = position;
+        // transform.position = position;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
